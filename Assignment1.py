5f44e66c1c3b09f0b6943f23f144f02bc6d7eef4
frances-ha <haire-f@ulster.ac.uk>
2021-06-24 22:41:10 +0100

    created gdf from df and set spatial reference information to Irish Grid

--- Assignment1.py
+++ Assignment1.py
@@ -1,10 +1,18 @@
-import sys
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
 
 df = pd.read_csv('DataFiles/PlanningEnforcement_AppealDecisions.csv')
-df['Geometry'] = list(zip(df['Y'], df['X']))
-df['Geometry'] = df['Geometry'].apply(Point)
-df.head()
+df['geometry'] = list(zip(df['X'], df['Y']))
+df['geometry'] = df['geometry'].apply(Point)
+del df['X'], df['Y']
+print(df.head())
+print(df.columns)
+
+gdf = gpd.GeoDataFrame(df)
+print(gdf.columns)
+gdf.set_crs("EPSG:29902", inplace=True)
+
+
+
 
