d9233795169bda4e8857ed8df1ded09ef0f203a8
frances-ha <haire-f@ulster.ac.uk>
2021-07-09 16:13:54 +0100

    wrote new function to add new df column based on lgd appeal success or failure

--- Assignment_Part_B.py
+++ Assignment_Part_B.py
@@ -8,7 +8,7 @@ from shapely.geometry import Point
 from cartopy.feature import ShapelyFeature
 import cartopy.crs as ccrs
 
-plt.ion()  # Turn on interactive mode
+# plt.ion()  # Turn on interactive mode
 
 # Write functions
 
@@ -31,6 +31,20 @@ def appeal_totals():
 
 def lgd_appeals():
     return join['LGDNAME'].value_counts()
+
+# Create new column to process existing data and return new variable
+
+def get_lgd_outcome():
+    lgd_outcome = []
+
+    for value in join['PAC_Decisi']:
+        if value == "Allowed":  # if appeal outcome ('PAC_Decisi') is allowed, this is a "fail" for the LGD.
+            lgd_outcome.append("Fail")
+        else:
+            lgd_outcome.append("Success")  # if appeal outcome is dismissed / withdrawn / varied / invalid (i.e. all
+        # other values), this is a "success" for the LGD.
+
+    join["LGD_Success_Fail"] = lgd_outcome
 # -----------------------------------------------------------
 # PREPARE TEST DATA FOR GIS ANALYSIS
 
@@ -45,39 +59,47 @@ print(lgd.columns.values)
 print(lgd.crs) # answer - epsg:29902
 lgd_wgs84 = lgd.to_crs(epsg=4326)
 
-# Apply spatial join then examine column names
+# Apply spatial join then inspect column names
 
 join = gpd.sjoin(appeal_outcomes, lgd_wgs84, how='inner', lsuffix='left', rsuffix='right')
 print(join.columns.values)
 
-# Amend gdf to only display columns of interest, then check output column names
+# Amend gdf to only display columns of interest to GIS analysis, then check output column names
 
 cols_of_interest = ['Appeal_Ref', 'LGDNAME', 'PAC_Decisi']
 join = join[cols_of_interest]
 print(join.columns.values)
 
-# Create a new column
+# Call function to create new column to summarise enforcement appeal outcomes into "Success" or "Failure" for LGD.
+
+print(get_lgd_outcome())
+print(join.head(10)) # quickly test if object has returned correct data
 
 # -------------------------------------------------------------------------
 # GIS ANALYSIS
 
 # How many enforcement appeals have been determined in Northern Ireland since 2013?
 
-total_appeals = join['PAC_Decisi'].count()
+total_appeals = new_join['PAC_Decisi'].count()
 print('{} total enforcement appeals'.format(total_appeals)) # answer - 129
 
 # How many unique values of appeal outcome are there, and what are these outcomes?
 
-num_appeals = len(join.PAC_Decisi.unique())
+num_appeals = len(new_join.PAC_Decisi.unique())
 print('{} unique classes of appeal outcome'.format(num_appeals)) # answer - 5
 
-outcomes_list = get_unique_appeals(join.PAC_Decisi)
-print('list of appeal outcomes: {}'.format(outcomes_list))
+outcomes_list = get_unique_appeals(new_join.PAC_Decisi)
+print('List of appeal outcomes: {}'.format(outcomes_list))
 
 # What are the overall results of planning enforcement appeal outcomes in Northern Ireland?
 
-print('total numbers of each appeal outcome: {}'.format(appeal_totals()))
+print('Total numbers of each appeal outcome: {}'.format(appeal_totals()))
 
 # Generate a list in descending order of total appeal cases dealt with per LGD
 
 print('Enforcement appeals handled per LGD in descending order: {}'.format(lgd_appeals()))
+
+# For each lgd, what is the percentage of allowed ("Failure") vs dismissed/varied/withdrawn ("Success")
+# enforcement appeals?  Due to low test data numbers, use most prolific LGD (Newry, Mourne and Down aka NMD) as example
+
+
